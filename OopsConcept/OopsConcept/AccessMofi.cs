0429e262a3b68bc2b3707056d30e02f5d618d1f9
AD001\z004efdb <shreya.malviya@siemens.com>
2023-08-30 09:52:38 +0530

    [Shreya] Add: Uc1 - Public AM

---
+++ OopsConcept/OopsConcept/AccessMofi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class AccessMofi
+    {
+        public void PublicAM()
+        {
+            //Uc1
+            Console.WriteLine("Public AC");
+        }
+    }
+}
---
+++ OopsConcept/OopsConcept/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AccessMofi accessMofi = new AccessMofi();
+            accessMofi.PublicAM();
+            Console.ReadLine();
+        }
+    }
+}
